b2ceae28c4132617eed140d3aaa6c87339e00d96
Matt Bowyer <mattbowyer@Matts-MacBook-Air-2.local>
2016-12-26 20:39:19 -0500

    fixed model operations

--- scheduler/admin.py
+++ scheduler/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(ProgramLeader)
+admin.site.register(TripLeader)
 admin.site.register(Floater)
 admin.site.register(Assigment)
 admin.site.register(Schedule)
