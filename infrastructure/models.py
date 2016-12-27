cb4c8faf690a54d358051bdfb8cf019d55182d38
Matt Bowyer <mattbowyer@Matts-MacBook-Air-2.local>
2016-12-27 00:35:51 -0500

    edited activity description

--- infrastructure/models.py
+++ infrastructure/models.py
@@ -23,7 +23,7 @@ class Activity(models.Model):
     availability = models.ManyToManyField(Period, blank=True,)
 
     def __str__(self):
-        return self.name + ' (' + self.capacity + ')'
+        return self.name + ' (' + str(self.capacity) + ')'
 
 class Zone(models.Model):
     name = models.CharField(max_length=30)
