0fcbec7e1761c0dea07862dc14bd9be7ffcb188e
Matt Bowyer <mattbowyer@Matts-MacBook-Air-2.local>
2017-01-13 12:31:29 -0500

    Adjusted Model

--- scheduler/models.py
+++ scheduler/models.py
@@ -9,7 +9,7 @@ class Schedule(models.Model):
         return self.day.__str__()
 
 class Assigment(models.Model):
-    schedule = models.ForeignKey(Scheulde, on_delete=models.CASCADE)
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
     period = models.ForeignKey(infrastructure_models.Period, on_delete=models.CASCADE)
     group = models.ForeignKey(operations_models.Group, on_delete=models.CASCADE)
     activity = models.ForeignKey(infrastructure_models.Activity, on_delete=models.CASCADE)
@@ -17,14 +17,14 @@ class Assigment(models.Model):
         return group.__str__() + ' <--> ' + activity.__str__() + ' <--> ' + Period.__str__()
 
 class GroupLeader(models.Model):
-    schedule = models.ForeignKey(Scheulde, on_delete=models.CASCADE)
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
     employee = models.ForeignKey(operations_models.Employee, on_delete=models.CASCADE)
     group = models.ForeignKey(operations_models.Group, on_delete=models.CASCADE)
     def __str__(self):
         return leader.__str__() + ' <--> ' + group.__str__()
 
 class Floater(models.Model):
-    schedule = models.ForeignKey(Scheulde, on_delete=models.CASCADE)
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
     employee = models.ForeignKey(operations_models.Employee, on_delete=models.CASCADE)
     activity = models.ForeignKey(infrastructure_models.Activity, on_delete=models.CASCADE)
     def __str__(self):
