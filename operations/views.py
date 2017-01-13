7fb2b20fe74e5393fc165c81c76e6bf255d022c7
Matt Bowyer <mattbowyer@Matts-MacBook-Air-2.local>
2017-01-13 17:02:36 -0500

    Removed API

--- operations/views.py
+++ operations/views.py
@@ -3,9 +3,10 @@ from rest_framework.response import Response
 
 from . import models
 from . import serializers
-
+'''
 class ListEmployee(APIView):
     def get(self, request, format=None):
         employees = models.Employee.objects.all()
         serializer = serializers.EmployeeSerializer(employees, many=True)
         return Response(serializer.data)
+'';
