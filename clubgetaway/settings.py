3a4b8f044b9f6378a5ebbd6e86cb0bb9ff891658
Matt Bowyer <mattbowyer@Matts-MacBook-Air-2.local>
2017-01-13 12:46:26 -0500

    Cleaned Project

--- clubgetaway/settings.py
+++ clubgetaway/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'PASSWORD': 'clubgetaway4$DEV',
         'HOST': 'localhost',
         'PORT': '',
-    }
+    },
     'testing': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
