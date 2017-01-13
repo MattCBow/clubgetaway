0d9a90ba65019b9dc940e830c84ca1ad5bed4cb9
Matt Bowyer <mattbowyer@Matts-MacBook-Air-2.local>
2017-01-13 12:32:34 -0500

    Adjusted Model

--- scheduler/admin.py
+++ scheduler/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(TripLeader)
-admin.site.register(Floater)
-admin.site.register(Assigment)
 admin.site.register(Schedule)
+admin.site.register(Assigment)
+admin.site.register(GroupLeader)
+admin.site.register(Floater)
