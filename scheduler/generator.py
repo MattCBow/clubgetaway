aea61fc4d74bf66b81f3f87db2488835c7b7b85c
Matt Bowyer <mattbowyer@Matts-MacBook-Air-2.local>
2017-01-16 01:01:36 -0500

    Debugging Scheduler

--- scheduler/generator.py
+++ scheduler/generator.py
@@ -211,8 +211,8 @@ def calculate_hueristics(schedule, period, group, zones):
         f[zone]['vacancy'] = (1.0*f[zone]['capacity'] - 1.0*f[zone]['visitors']) / f[zone]['capacity']
         f[zone]['proximity'] = 1.0
         if period != 0:
-            f[zone]['proximity'] = zones[prev_zone]['proximity'][zone]
-        if f[zone]['vacancy'] != 0.0 and f[zone]['visits'] == 0.0 and f[zone]['proximity'] == 1.0:
+            f[zone]['proximity'] = 1.0 - 1.0*zones[prev_zone]['proximity'][zone]/5
+        if f[zone]['vacancy'] != 0.0 and f[zone]['visits'] == 0.0:
             h[zone] = (1.0*f[zone]['proximity']) + (1.0*f[zone]['vacancy']) + (1.0*f[zone]['level'])
     return h, f
 
