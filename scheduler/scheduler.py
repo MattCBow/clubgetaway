fdd07b9d43e47ea7105eccf7b790b299d4025ffc
Matt Bowyer <mattbowyer@nbp-216-123.nbp.ruw.rutgers.edu>
2017-02-17 03:07:17 -0500

    Changed Zones

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -231,7 +231,7 @@ def print_schedule(schedule, zones):
         'Mountain View Field' :'Mountain View Field',
     }
 
-    visits = [[0 for z in range(len(zones)) ] for p in range(len(period))]
+    visits = [[0 for z in range(len(zones)) ] for p in range(len(schedule))]
     for p in range(period):
         for g in range(group):
             z = schedule[p][g]
