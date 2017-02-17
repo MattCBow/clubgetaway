12d14115f4b55bb7847eed1e4e5ea6cc770b7909
Matt Bowyer <mattbowyer@nbp-216-123.nbp.ruw.rutgers.edu>
2017-02-17 02:13:26 -0500

    Changed Zones

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -240,7 +240,7 @@ def print_schedule(schedule, factors):
             fct = factors[period][group][ass]
             if ass != None:
                 fct = factors[period][group][ass]
-                print nickname[ass], '[', str(fct['visitors']), '/', str(fct['capacity']), ']', '\t',
+                print nickname[ass], ('['+str(int(fct['visitors']))+'/'+str(int(fct['capacity']))+']'), '\t',
             else:
                 print '\t\t\t',
 
