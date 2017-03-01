4fc784bf28d7e6a18951de0245196de46cdb124b
Matt Bowyer <mattbowyer@Matts-Air-2.home>
2017-03-01 14:48:59 -0500

    Added File Field

--- operations/models.py
+++ operations/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from infrastructure.models import *
+import datetime
 
 class Program(models.Model):
     YOUTH_PROGRAM = 'YP'
