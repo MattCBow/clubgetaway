753fd3df9fc65f96719939e44ee94c70b20cdce0
Matt Bowyer <mattbowyer@Matts-Air-2.home>
2017-03-01 14:27:42 -0500

    Removed Scheduler implemented CSV

--- scheduler/admin.py
+++ scheduler/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-
-from .models import *
-
-admin.site.register(Schedule)
-admin.site.register(Assigment)
-admin.site.register(GroupLeader)
-admin.site.register(Floater)
